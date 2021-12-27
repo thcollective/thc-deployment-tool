5408dcb0da9226be5bb31cf6a942d791ff3ceb93
ricobl4ck <adrishahri@gmail.com>
2021-12-27 15:17:26 +0800

    reformat TODO

--- main.go
+++ main.go
@@ -245,7 +245,7 @@ func main() {
 
 	if ansSentry == "Yes" {
 
-		// TODO: need to add some properties file depends on project/framework their working
+		// TODO need to add some properties file depends on project/framework their working
 
 		folderPathSentry := ".github/workflows"
 		os.MkdirAll(folderPathSentry, os.ModePerm)
@@ -269,8 +269,7 @@ func main() {
 
 	/* END SENTRY GITHUB ACTIONS*/
 
-	// TODO: GITHUB ACTIONS for CI/CD : for which actions runs first
-	// fmt.Println("PHASE: CLOUD RUN GITHUB ACTIONS FILE CREATION")
+	// TODO GITHUB ACTIONS for CI/CD : for which actions runs first
 
 	/* START CLOUD RUN GITHUB ACTIONS*/
 	fmt.Println("PHASE: CLOUD RUN GITHUB ACTIONS FILE CREATION")
@@ -421,7 +420,7 @@ func main() {
 
 	// _, answer8, err := env.Run()
 
-	// // TODO: these needs to be an array if env > 1 | push object to array
+	// TODO these needs to be an array if env > 1 | push object to array
 	// envNames := ""
 	// envValues := ""
 	// answerEnv := ""
